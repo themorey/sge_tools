587e48c69e1cef7f7116e41c73577e78d018632f
Jerry Morey <24438151+themorey@users.noreply.github.com>
2021-02-04 12:27:24 -0500

    updated
    Removed value from 'maxvmem' field instead of converting (line 30)
    Added numpy import
    Added line 38 (replacing line 37) to fix "ValueError: cannot convert float NaN to integer"

--- sge_tools.py
+++ sge_tools.py
@@ -1,7 +1,8 @@
 def sge_qacct_parse(filename):
     # Import libraries
     import pandas as pd
-    
+    import numpy as np
+
     # Parse qacct log file into dictionary 
     dicts=[]
     with open(filename, 'r') as f: 
@@ -21,19 +22,20 @@ def sge_qacct_parse(filename):
     # Load data into dataframe and set correct types
     df = pd.DataFrame(dicts)
     floatfields = ['priority','ru_wallclock','ru_utime','ru_stime','ru_maxrss','ru_ixrss','ru_ismrss','ru_idrss',               'ru_isrss','ru_minflt','ru_majflt','ru_nswap','ru_inblock','ru_oublock','ru_msgsnd','ru_msgrcv',               'ru_nsignals','ru_nvcsw','ru_nivcsw','cpu','mem','io','iow','maxvmem']
-    convert2Gb = {'T':1024.,'G':1.,'M':1./1024.,'K':1./1024./1024}
+    ### convert2Gb = {'T':1024.,'G':1.,'M':1./1024.,'K':1./1024./1024}
     for field in floatfields: 
         try: 
             df[field] = df[field].astype('float')
         except ValueError:
-            df[field] = df[field].apply(lambda s: float(s[:-1])*convert2Gb[s[-1]])
+            df[field] = df[field].str.strip('GMTK')
             df[field] = df[field].astype('float')
     datefields = ['qsub_time','start_time','end_time']
     for field in datefields: 
         df[field] = pd.to_datetime(df[field])  #,format='%m/%d/%y %I:%M%p'
     intfields = ['slots','jobnumber','exit_status'] 
     for field in intfields: 
-        df[field] = df[field].astype('int')
-
+        #df[field] = df[field].astype('int')
+        df[field] = df[field].astype(np.float).astype("Int32")
+        
     return df
 
